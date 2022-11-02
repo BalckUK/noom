caa3bba7981e976425db3388f25b2e82f1898063
UMK <haga5032@naver.com>
2022-11-02 16:51:41 +0900

    42p

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 //console.log("hello");
 
+import http from "http";
+import WebSocket from "ws";
 import express from 'express';
-import path from 'path';
+
 
 const app = express();
 const __dirname = "C:/Users/sjd/Desktop/vueproject/WebRTC/src";
@@ -15,4 +17,10 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+server.listen(3000, handleListen);
+
+//app.listen(3000, handleListen);
