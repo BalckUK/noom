d0fe662e0b277b3e8641968fe349426069de3a76
UMK <haga5032@naver.com>
2022-11-02 16:40:56 +0900

    36p

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
-console.log("hello");
+//console.log("hello");
 
 import express from 'express';
+import path from 'path';
 
 const app = express();
+const __dirname = "C:/Users/sjd/Desktop/vueproject/WebRTC/src";
+
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
 app.listen(3000, handleListen);
